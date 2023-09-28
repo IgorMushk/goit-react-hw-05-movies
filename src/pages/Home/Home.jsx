eaa46811ef3d0cbb72b5c71600ebcb5a129a9c76
IgorMushk <igor.mushketik@gmail.com>
2023-09-28 15:09:42 +0300

    Add for Home page fetchMovie
    !!! Network  Fetch/XHR  - 2X - Status  200

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { fetchMovies } from 'services/moviesAPI';
 
-const Home=()=> {
+const Home = () => {
+  const [trendingMovies, settrendingMovies] = useState([]);
+  //const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    //setLoading(true);
+    fetchMovies('/trending//movie/day?language=en-US')
+      .then(data => {
+        settrendingMovies(data.results);
+        //setLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, []);
   return (
+    <>
     <div>Home</div>
-  )
-}
+     {/* {loading && <Loader />} */}
+     <h1>Trending today</h1>
+     <ul>
+        {trendingMovies.map(movie => (
+            <li key={movie.id}> {movie.title}</li>
+            ))}
+     </ul>
+    </>
+  );
+};
 
 export default Home;
