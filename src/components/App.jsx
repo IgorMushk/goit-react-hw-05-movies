cf6584c63075d56d010e36353175a220f5a528a2
IgorMushk <igor.mushketik@gmail.com>
2023-09-28 12:22:24 +0300

    add Logo TMDB

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import Home from 'pages/Home/Home';
 import MovieDetails from 'pages/MovieDetails/MovieDetails';
 import Movies from 'pages/Movies/Movies';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link, NavLink, Route, Routes } from 'react-router-dom';
 import Cast from './Cast/Cast';
 import Review from './Review/Review';
 
@@ -9,8 +9,16 @@ export const App = () => {
   return (
     <div>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/movies">Movies</Link>
+        <Link to="/">
+          <img
+            src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
+            alt="The Movie Database (TMDB)"
+            width="154"
+            height="20"
+          ></img>
+        </Link>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
