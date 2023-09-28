4dadbcfa18000d18f5a74689ab833563878b8361
IgorMushk <igor.mushketik@gmail.com>
2023-09-28 11:49:54 +0300

    Add - Routes pages components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import Home from 'pages/Home/Home';
+import MovieDetails from 'pages/MovieDetails/MovieDetails';
+import Movies from 'pages/Movies/Movies';
+import { Link, Route, Routes } from 'react-router-dom';
+import Cast from './Cast/Cast';
+import Review from './Review/Review';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template!
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/movies">Movies</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Review />} />
+        </Route>
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Routes>
     </div>
   );
 };
